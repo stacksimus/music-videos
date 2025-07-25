0fcfe68394f17bcb1d5740f9b04ce17866c5337d
Stephen Kam <kam.stephen@gmail.com>
2025-07-25 17:53:21 -0400

    updates

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 const modal = document.getElementById("modal");
 const modalBackground = document.getElementById("modal-background");
 
-const btn = document.getElementById("avril-lavigne_complicated");
+Array.from(document.getElementsByClassName('card')).map(card => {
+    const youtubeId = card.getAttribute('youtube-id');
+    
+    // When the user clicks the button, open the modal
+    card.onclick = function() {
+        modalBackground.style.display = "block";
+        modal.style.display = "block";
+        const iframe = document.createElement('iframe');
+        iframe.id = 'youtube-iframe';
+        iframe.src = `https://www.youtube.com/embed/${youtubeId}`;
+        iframe.frameborder = '0';
+        iframe.width = '99%';
+        iframe.height = '98%';
+        iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share";
+        iframe.referrerpolicy='strict-origin-when-cross-origin';
+        iframe.allowfullscreen = true;
+        modal.appendChild(iframe);
+    }
+});
 
-// When the user clicks the button, open the modal
-btn.onclick = function() {
-  modalBackground.style.display = "block";
-  modal.style.display = "block";
-  const iframe = document.createElement('iframe');
-  iframe.id = 'youtube-iframe';
-  iframe.src = 'https://www.youtube.com/embed/TIy3n2b7V9k?si=vSVsO8VIocLVFBHR'; // Set the URL you want to load in the iframe
-  iframe.frameborder = '0';
-  iframe.width = '99%';
-  iframe.height = '98%';
-  iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share";
-  iframe.referrerpolicy='strict-origin-when-cross-origin';
-  iframe.allowfullscreen = true;
-  modal.appendChild(iframe);
-}
+
+const btn = document.getElementById("avril_lavigne-complicated");
+
+
 
 modalBackground.onclick = function() {
   modalBackground.style.display = "none";
